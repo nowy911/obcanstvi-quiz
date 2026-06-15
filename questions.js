a1d430bb9887b3aba0d6ac4a0b71c46448ca14bf
nowy911 <65302553+nowy911@users.noreply.github.com>
2026-06-15 20:34:17 +0200

    Update questions.js
    new questions

--- questions.js
+++ questions.js
@@ -1,1 +1,22 @@
-const QUESTIONS = [];
+const QUESTIONS = [
+{
+  question: "Kdo se může stát prezidentem České republiky?",
+  options: {
+    A: "18 let",
+    B: "21 let",
+    C: "26 let",
+    D: "40 let"
+  },
+  correct: "D"
+},
+{
+  question: "Které komory tvoří Parlament České republiky?",
+  options: {
+    A: "Senát a vláda",
+    B: "Senát a ministerstva",
+    C: "Poslanecká sněmovna a vláda",
+    D: "Poslanecká sněmovna a Senát"
+  },
+  correct: "D"
+}
+];
